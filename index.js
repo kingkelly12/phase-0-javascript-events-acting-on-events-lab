0706b451982cb292892b3eab776dc9d8319f0603
Kelly Koome <koomekelly7@gmail.com>
2025-03-15 17:11:48 +0300

    acting on events

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+
+    }
+  }
+  function moveDodgerLeft() {
+    const dodger = document.getElementById("dodger");
+    let left = parseInt(dodger.style.left.replace("px", ""), 10);
+
+    if (left > 0) { // Prevent moving out of bounds
+        dodger.style.left = `${left - 1}px`;
+    }
+}
